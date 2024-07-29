57a1585e766752a31609e5f03130a5401d63e3fc
luismartinezkyb <18030545@itcelaya.edu.mx>
2024-07-28 20:55:06 -0600

    (feat): Implementación de products module with TCP configuration envs variables

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -4,10 +4,14 @@ import * as joi from 'joi';
 interface EnvVars {
   PORT: number;
   DATABASE_URL: string;
+  PRODUCTS_MICROSERVICE_HOST: string;
+  PRODUCTS_MICROSERVICE_PORT: number;
 }
 const envsSchema = joi
   .object({
     PORT: joi.number().required(),
+    PRODUCTS_MICROSERVICE_HOST: joi.string().required(),
+    PRODUCTS_MICROSERVICE_PORT: joi.number().required(),
   })
   .unknown(true);
 
@@ -22,4 +26,6 @@ if (error) {
 }
 export const envs = {
   port: envVars.PORT,
+  microservice_products_host: envVars.PRODUCTS_MICROSERVICE_HOST,
+  microservice_products_port: envVars.PRODUCTS_MICROSERVICE_PORT,
 };
